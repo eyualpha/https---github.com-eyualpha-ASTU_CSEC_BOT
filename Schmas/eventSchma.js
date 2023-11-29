d008ccf320afe1ab86fe7a17e84f9f5c521ce5c0
eyualpha <143643131+eyualpha@users.noreply.github.com>
2023-11-30 00:21:07 +0300

    schmas update

--- Schmas/eventSchma.js
+++ Schmas/eventSchma.js
@@ -1,8 +1,7 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const eventSchema = new mongoose.Schema({
-    topic: String,
-    detail: String,
-    id:String
-})
+  topic: String,
+  detail: String,
+  id: String,
+});
 module.exports = mongoose.model("Event", eventSchema);
-
